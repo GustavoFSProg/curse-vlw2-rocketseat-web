d20bc205a198de5139a602d79457f5aea797e65c
Gustavo <gustavosohne38@gmail.com>
2020-08-03 17:51:57 -0300

    curso-web-landing page

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Logo from '../../assets/images/logo.svg'
+import imagemLanding from '../../assets/images/landing.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import study from '../../assets/images/icons/study.svg'
+import './styles.css'
+import giveClasses from '../../assets/images/icons/give-classes.svg'
+
+export default function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={Logo} alt="logo" />
+        </div>
+        <img src={imagemLanding} alt="landing-img" className="hero-image" />
+
+        <div className="buttons-container">
+          <a href="#" className="study">
+            <img src={giveClasses} alt="Estudar" />
+            Estudar
+          </a>
+
+          <a href="#" className="give-classes">
+            <img src={study} alt="study-icon" />
+            Dar Aulas
+          </a>
+        </div>
+
+        <span className="total-conections">
+          Total de 200 conexoes já realizadas!
+          <img src={purpleHeartIcon} alt="heart-icon" />
+        </span>
+      </div>
+    </div>
+  )
+}
